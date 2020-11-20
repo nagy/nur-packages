0649ee22199bd49a84ef9137593e8e561e847566
Daniel Nagy <danielnagy@posteo.de>
2020-11-20 11:41:26 +0100

    hyperspec: move into own file

--- default.nix
+++ default.nix
@@ -1,21 +1,4 @@
 { pkgs ? import <nixpkgs> {}  }:
-let
-  hyperspec = pkgs.stdenv.mkDerivation {
-    pname = "hyperspec";
-    version = "7.0";
-    meta.description = "The Common Lisp HyperSpec";
-    src = builtins.fetchurl {
-      url = http://ftp.lispworks.com/pub/software_tools/reference/HyperSpec-7-0.tar.gz;
-      sha256 = "1hyphnx74d595qz692qh8fc2xk8v6zaclqhji3cdp5y6kmm6dh8s";
-    };
-    # we skip the unpackPhase
-    unpackPhase = "true";
-    installPhase = ''
-        mkdir -p "$out/share/hyperspec/"
-        tar -C "$out/share/hyperspec/" -xzvf $src
-    '';
-  };
-in
 {
-  inherit hyperspec;
+  hyperspec = pkgs.callPackage ./pkgs/hyperspec { } ;
 }
