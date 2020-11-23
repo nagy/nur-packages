d9b33ba604c8fb34e61a1bda06c79c7dca099b95
Daniel Nagy <danielnagy@posteo.de>
2020-11-23 22:41:26 +0100

    hyperspec: fix syntax

--- pkgs/hyperspec/default.nix
+++ pkgs/hyperspec/default.nix
@@ -5,7 +5,7 @@ stdenv.mkDerivation {
   meta = {
     description = "The Common Lisp HyperSpec";
     homepage = "http://www.lispworks.com/documentation/HyperSpec/Front/index.htm";
-  }
+  };
   src = builtins.fetchurl {
     url = http://ftp.lispworks.com/pub/software_tools/reference/HyperSpec-7-0.tar.gz;
     sha256 = "1hyphnx74d595qz692qh8fc2xk8v6zaclqhji3cdp5y6kmm6dh8s";
