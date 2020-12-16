bce84c5bbce69fa4fd58d9b7d3256458fced7b50
Daniel Nagy <danielnagy@posteo.de>
2020-12-16 23:51:59 +0100

    luaPackages.tl: 0.7.1 -> 0.9.0

--- pkgs/teal.nix
+++ pkgs/teal.nix
@@ -1,30 +1,19 @@
-{ stdenv, luaPackages, fetchFromGitHub, makeWrapper }:
-
+{ stdenv, lua53Packages, fetchurl, makeWrapper }:
 let
-  inherit (luaPackages) lua buildLuaPackage;
-in buildLuaPackage rec {
-  name = "teal";
-  version = "0.7.1";
-  src = builtins.fetchurl {
-    url = "https://teal-lang.org/downloads/teal-${version}";
-    sha256 = "01f0xfp9g5z6jhdr006n15bnn0rlcfdbgiilamqgkhx28gnjrswf";
+  inherit (lua53Packages) lua luaOlder luaAtLeast buildLuarocksPackage;
+in buildLuarocksPackage rec {
+  pname = "tl";
+  version = "0.9.0-1";
+  src = fetchurl {
+    url = "https://luarocks.org/manifests/hisham/tl-0.9.0-1.src.rock";
+    sha256 = "012ma153fprx0k8dh8pr01232wcgslwp1gycj53zlbrizldf36gd";
   };
-  unpackPhase = "true";
-  buildPhase = "true";
-
-  nativeBuildInputs = [ makeWrapper ];
-
-  installPhase = ''
-    install -Dp $src $out/lib/lua/${lua.luaversion}/teal.lua
-
-    install -D $src $out/bin/teal
-    wrapProgram $out/bin/teal \
-      --prefix LUA_PATH ";" "$out/lib/lua/${lua.luaversion}/?.lua"
-  '';
+  disabled = (luaOlder "5.3") || (luaAtLeast "5.5");
+  propagatedBuildInputs = with lua53Packages ; [ compat53 argparse luafilesystem ];
 
   meta = with stdenv.lib; {
     description = "teal is a lua that compiles to Lua";
-    homepage = https://teal-lang.org/;
+    homepage = https://github.com/teal-language/tl/;
     license = licenses.mit;
   };
 }
