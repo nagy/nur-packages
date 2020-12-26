a553947855c34fc20bf04f01d0b45f190cc3efe3
Daniel Nagy <danielnagy@posteo.de>
2020-12-26 16:21:35 +0100

    luaPackages.fennel: 0.6.0 -> 0.7.0

--- pkgs/fennel.nix
+++ pkgs/fennel.nix
@@ -4,10 +4,10 @@ let
   inherit (luaPackages) lua buildLuaPackage;
 in buildLuaPackage rec {
   name = "fennel";
-  version = "0.6.0";
+  version = "0.7.0";
   src = builtins.fetchurl {
     url = "https://fennel-lang.org/downloads/fennel-${version}";
-    sha256 = "01f0xfp9g5z6jhdr006n15bnn0rlcfdbgiilamqgkhx28gnjrswf";
+    sha256 = "1j521x3qqxvrfard26abb1hpvfh9y245ha0kn4hz0cpmyqsw92q6";
   };
   unpackPhase = "true";
   buildPhase = "true";
