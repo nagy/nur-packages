5c7d8b422e30d095b12ad3bd287bf51eb3249cdd
Daniel Nagy <danielnagy@posteo.de>
2020-12-26 21:57:29 +0100

    schemaorg: nixfmt

--- pkgs/schemaorg/default.nix
+++ pkgs/schemaorg/default.nix
@@ -2,18 +2,21 @@
 stdenv.mkDerivation rec {
   pname = "schemaorg";
   version = "10.0";
-  meta = {
-    description = "schema.org";
-    homepage = "schema.org";
-  };
+
   src = fetchFromGitHub {
     owner = "schemaorg";
     repo = "schemaorg";
     rev = "V${version}-release";
     sha256 = "15p11fq0v51rk804wwz6z48zwd9r22m470ly9qly9gpjb30x47j4";
   };
+
   installPhase = ''
-      mkdir -p "$out/share/schema.org/"
-      cp -r "data/releases/${version}/" $out/share/schema.org/
+    mkdir -p "$out/share/schema.org/"
+    cp -r "data/releases/${version}/" "$out/share/schema.org/"
   '';
+
+  meta = {
+    description = "schema.org";
+    homepage = "schema.org";
+  };
 }
