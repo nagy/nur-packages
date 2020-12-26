ddffc8c85f86f74e9e96c6e1f044c55e140a40f8
Daniel Nagy <danielnagy@posteo.de>
2020-12-26 22:22:41 +0100

    apertium: 3.6.3 -> 3.7.1

--- pkgs/apertium.nix
+++ pkgs/apertium.nix
@@ -1,17 +1,14 @@
-{ stdenv, fetchFromGitHub, callPackage , autoconf, automake, libtool, libxml2,
-libxslt , pkg-config, flex, pcre, pcre-cpp, icu }:
+{ stdenv, fetchFromGitHub, callPackage, autoconf, automake, libtool, libxml2
+, libxslt, pkg-config, flex, pcre, pcre-cpp, icu }:
 stdenv.mkDerivation rec {
   pname = "apertium";
-  version = "3.6.3";
+  version = "3.7.1";
 
   src = fetchFromGitHub {
     owner = "apertium";
     repo = "apertium";
-    # rev = "v${version}";
-    # we have to skip forward a couple of commits ahead of 3.6.3 because there
-    # it shipped with the removal of internet dependencies.
-    rev = "7c9a28b1ce0033c7e8d700b5076258da78870116";
-    sha256 = "0lr201a2mrs6fhg5ir2x838aayl2plq7rvp4ddpv8jrqqmmk62in";
+    rev = "v${version}";
+    sha256 = "02cvf9dhg13ml1031apkfaygbm1qvcgh9v1k7j9yr3c7iww3hywf";
   };
 
   buildInputs = [ autoconf automake libtool libxml2 libxslt flex pkg-config pcre pcre-cpp icu
@@ -24,7 +21,7 @@ stdenv.mkDerivation rec {
 
   meta = {
     description = "A free/open-source machine translation platform";
-    homepage = https://github.com/apertium/apertium;
+    homepage = "https://github.com/apertium/apertium";
 
     license = stdenv.lib.licenses.gpl2;
     maintainers = [  ];
