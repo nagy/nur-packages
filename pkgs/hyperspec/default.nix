f3aa4a68aee1a9bd85663f4ea3b70e5286fc20de
Daniel Nagy <danielnagy@posteo.de>
2020-12-27 22:00:48 +0100

    hyperspec: pass fetchurl as an argument

--- pkgs/hyperspec/default.nix
+++ pkgs/hyperspec/default.nix
@@ -1,4 +1,4 @@
-{ stdenv } :
+{ stdenv, fetchurl }:
 stdenv.mkDerivation {
   pname = "hyperspec";
   version = "7.0";
@@ -6,8 +6,8 @@ stdenv.mkDerivation {
     description = "The Common Lisp HyperSpec";
     homepage = "http://www.lispworks.com/documentation/HyperSpec/Front/index.htm";
   };
-  src = builtins.fetchurl {
-    url = http://ftp.lispworks.com/pub/software_tools/reference/HyperSpec-7-0.tar.gz;
+  src = fetchurl {
+    url = "http://ftp.lispworks.com/pub/software_tools/reference/HyperSpec-7-0.tar.gz";
     sha256 = "1hyphnx74d595qz692qh8fc2xk8v6zaclqhji3cdp5y6kmm6dh8s";
   };
   # we skip the unpackPhase
