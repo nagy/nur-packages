707d50ea5b41f0ec8307f45927942fb5f5531100
Daniel Nagy <danielnagy@posteo.de>
2021-02-06 19:03:17 +0100

    ticker: 2.3.0 -> 2.4.0

--- pkgs/ticker/default.nix
+++ pkgs/ticker/default.nix
@@ -1,13 +1,13 @@
 { lib, buildGoModule, fetchFromGitHub }:
 buildGoModule rec {
   pname = "ticker";
-  version = "2.3.0";
+  version = "2.4.0";
 
   src = fetchFromGitHub {
     owner = "achannarasappa";
     repo = pname;
     rev = "v${version}";
-    sha256 = "0cyp29qng0pbp2xlbj172pmrkvv24l07k1spqw99m7724bg28i2c";
+    sha256 = "1kp1wgb3bakkawr81sa7fsx8gaazica8lffvysqcp5d5kw22jirv";
   };
 
   vendorSha256 = "12gkwqkf7xxgc3ckra7ar6n4klykja43ya0s1j0ds3pinyrhvc5b";
