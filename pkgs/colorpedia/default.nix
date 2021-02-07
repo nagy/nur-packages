8c6c748b6d2f4fa32abc7fcaf2383b2cfba75add
Daniel Nagy <danielnagy@posteo.de>
2021-02-07 22:33:11 +0100

    colorpedia: add shell completion files

--- pkgs/colorpedia/default.nix
+++ pkgs/colorpedia/default.nix
@@ -1,4 +1,5 @@
-{ lib, fetchPypi, python3Packages, setuptools, setuptools_scm, fire, toml }:
+{ lib, fetchPypi, python3Packages, setuptools, setuptools_scm, fire, toml
+, installShellFiles }:
 
 python3Packages.buildPythonApplication rec {
   pname = "colorpedia";
@@ -9,9 +10,18 @@ python3Packages.buildPythonApplication rec {
     sha256 = "1b19mlss6arc4sk0bsk6ssqc6advnzr2kq7snxrv8a6hki81ykm5";
   };
 
-  nativeBuildInputs = [ setuptools_scm ];
+  nativeBuildInputs = [ setuptools_scm installShellFiles ];
 
   propagatedBuildInputs = [ fire toml setuptools ];
+
+  postInstall = ''
+    installShellCompletion --cmd color \
+           --bash <($out/bin/color -- --completion) \
+           --zsh <($out/bin/color -- --completion | sed "s/:/: /g")
+    installShellCompletion --cmd colorpedia \
+           --bash <($out/bin/colorpedia -- --completion) \
+           --zsh <($out/bin/colorpedia -- --completion | sed "s/:/: /g")
+  '';
 
   meta = with lib; {
     description = "Command-line tool for looking up colors and palettes";
