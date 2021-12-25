976784ff4bd862955359b28998d3740c55f85935
Daniel Nagy <danielnagy@posteo.de>
2021-12-26 00:18:42 +0100

    Make flake available for more systems

--- flake.nix
+++ flake.nix
@@ -1,8 +1,12 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  outputs = { self, nixpkgs, nur }: rec {
-    packages.x86_64-linux = import self {
-      pkgs = import nixpkgs { system = "x86_64-linux"; };
-    };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in with pkgs; rec {
+        packages = import self { inherit pkgs; };
+      });
 }
