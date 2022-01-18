9ed441c5473793edd84de31a3d206d99fbe57ec5
Daniel Nagy <danielnagy@posteo.de>
2022-01-19 00:07:43 +0100

    libvosk: update source

--- pkgs/libvosk/default.nix
+++ pkgs/libvosk/default.nix
@@ -10,9 +10,11 @@ stdenv.mkDerivation rec {
     # use my fork which has some NixOS fixes.
     owner = "nagy";
     repo = "vosk-api";
-    rev = "6e2e590e43e5d8495e5edcfa0523850111c487fe";
-    hash = "sha256-D5uxjqZxJTNMj0oquhHfsacQDArSBoBHxuJeN2xfPIk=";
-  }))+"/src/";
+    rev = "524ef936edeaf7f609fba8a6a6ce22315d151f22";
+    hash = "sha256-dJmmx094Vxs0t8Km472CjaeMsF2mjEhyBR1iwhF4xMg=";
+  })) + "/src/";
+
+  makeFlags = [ "PREFIX=$(out)" ];
 
   KALDI_ROOT = kaldi.outPath;
 
@@ -21,7 +23,8 @@ stdenv.mkDerivation rec {
   HAVE_OPENBLAS_CLAPACK = "0";
 
   meta = with lib; {
-    description = "Offline speech recognition API for Android, iOS, Raspberry Pi and servers with Python, Java, C# and Node";
+    description =
+      "Offline speech recognition API for Android, iOS, Raspberry Pi and servers with Python, Java, C# and Node";
     homepage = "https://github.com/alphacep/vosk-api";
     license = with licenses; [ asl20 ];
   };
