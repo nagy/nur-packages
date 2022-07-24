eaafb9625b4775a06abff21860e2a8f3050bb596
Daniel Nagy <danielnagy@posteo.de>
2022-07-24 15:50:52 +0200

    lttoolbox: 3.5.4 -> 3.6.6

--- pkgs/lttoolbox.nix
+++ pkgs/lttoolbox.nix
@@ -1,20 +1,37 @@
 { stdenv, lib, fetchFromGitHub, autoconf, automake, libtool, libxml2, libxslt
-, pkg-config, flex, pcre, pcre-cpp, autoreconfHook }:
+, pkg-config, flex, pcre, pcre-cpp, autoreconfHook, icu, utf8cpp }:
 stdenv.mkDerivation rec {
   pname = "lttoolbox";
-  version = "3.5.4";
+  version = "3.6.6";
 
   src = fetchFromGitHub {
     owner = "apertium";
     repo = "lttoolbox";
     rev = "v${version}";
-    sha256 = "sha256-FK5Stq+fzGCjL0Dq5Wg3vpNzVB9e56QPx/5dOKACjxk=";
+    sha256 = "sha256-dEmlJ83JfMAyMeRcTh/Ca6+OLeqKxuHBhp1G5SCeJWE=";
   };
 
   nativeBuildInputs = [ autoreconfHook pkg-config ];
 
-  buildInputs =
-    [ autoconf automake libtool libxml2 libxslt flex pcre pcre-cpp ];
+  buildInputs = [
+    icu
+    utf8cpp
+    autoconf
+    automake
+    libtool
+    libxml2
+    libxslt
+    flex
+    pcre
+    pcre-cpp
+  ];
+
+  postPatch = ''
+    for file in lttoolbox/*.h lttoolbox/*.cc ; do
+      substituteInPlace $file \
+          --replace "<utf8.h>" "<utf8cpp/utf8.h>"
+    done
+  '';
 
   meta = with lib; {
     description =
