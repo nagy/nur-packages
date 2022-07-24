d394584f96bfce677422bd7d1d4e359185d85cca
Daniel Nagy <danielnagy@posteo.de>
2022-07-24 15:08:14 +0200

    treewide: move files up one dir

--- default.nix
+++ default.nix
@@ -11,8 +11,6 @@ in (thePackages // rec {
 
   lib = callPackage ./lib.nix { };
 
-  hyperspec = callPackage ./pkgs/hyperspec { };
-
   luaPackages = lua53Packages;
 
   lua53Packages = recurseIntoAttrs {
@@ -35,56 +33,16 @@ in (thePackages // rec {
       blender-asset-tracer = py3.callPackage ./pkgs/blender-asset-tracer { };
     }));
 
-  schemaorg = callPackage ./pkgs/schemaorg { };
-
-  libetc = callPackage ./pkgs/libetc { };
-
-  gh-dash = callPackage ./pkgs/gh-dash { };
-
   lttoolbox = callPackage ./pkgs/lttoolbox { };
 
   apertium = callPackage ./pkgs/apertium { inherit lttoolbox; };
-
-  lunasvg = callPackage ./pkgs/lunasvg { };
 
   lispPackages = recurseIntoAttrs {
     vacietis = callPackage ./pkgs/vacietis { };
     dbus = callPackage ./pkgs/cl-dbus { };
   };
 
-  rustfilt = callPackage ./pkgs/rustfilt { };
-
-  bollux = callPackage ./pkgs/bollux { };
-
-  gemget = callPackage ./pkgs/gemget { };
-
-  cpp-httplib = callPackage ./pkgs/cpp-httplib { };
-
-  cxxtimer = callPackage ./pkgs/cxxtimer { };
-
-  cxxmatrix = callPackage ./pkgs/cxxmatrix { };
-
-  hackernews-tui = callPackage ./pkgs/hackernews-tui { };
-
-  har-tools = callPackage ./pkgs/har-tools { };
-
-  ksuid = callPackage ./pkgs/ksuid { };
-
-  wagi = callPackage ./pkgs/wagi { };
-
-  bindle = callPackage ./pkgs/bindle { };
-
-  pigo = callPackage ./pkgs/pigo { };
-
-  htmlq = callPackage ./pkgs/htmlq { };
-
   libvosk = callPackage ./pkgs/libvosk { };
-
-  s-dot = callPackage ./pkgs/s-dot { };
-
-  s-dot2 = callPackage ./pkgs/s-dot2 { };
-
-  tinmop = callPackage ./pkgs/tinmop { };
 
   cl-opengl = callPackage ./pkgs/cl-opengl { };
 
@@ -92,19 +50,5 @@ in (thePackages // rec {
 
   ksv = callPackage ./pkgs/ksv { };
 
-  sasl2-oauth = callPackage ./pkgs/sasl2-oauth { inherit sasl2-oauth; };
-
-  oauth2ms = pkgs.callPackage ./pkgs/oauth2ms { };
-
-  q = callPackage ./pkgs/q { };
-
-  snid = callPackage ./pkgs/snid { };
-
-  npt = callPackage ./pkgs/npt { };
-
-  rust-u2f = callPackage ./pkgs/rust-u2f { };
-
-  u8strings = callPackage ./pkgs/u8strings { };
-
-  bzip3 = callPackage ./pkgs/bzip3 { };
+  sasl2-oauth = callPackage ./pkgs/sasl2-oauth.nix { inherit sasl2-oauth; };
 })
