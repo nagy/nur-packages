145869690072532a41ece6acfaa97a3129900597
Daniel Nagy <danielnagy@posteo.de>
2023-05-07 13:35:41 +0200

    hackernews-tui: 0.10.2 -> 0.13.1
    Diff: https://github.com/aome510/hackernews-tui/compare/v0.10.2...v0.13.1

--- pkgs/hackernews-tui.nix
+++ pkgs/hackernews-tui.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "hackernews-tui";
-  version = "0.10.2";
+  version = "0.13.1";
 
   src = fetchFromGitHub {
     owner = "aome510";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-v/HM0oCgsGX4CqFKJyMhTqATvN5/TgolhJjjWnygRec=";
+    hash = "sha256-z5xe+cvkAzI7Dx3xxOhVZT4UYSsi1j/qdVghb+01dfg=";
   };
 
-  cargoSha256 = "sha256-CM14L9UykIT3KFMqtHhy3cYA4TR6pTEutkgi0gGDpZw=";
+  cargoSha256 = "sha256-Fk/3aWDlyU1J28JBLBknS9GCDClar05Lt3JgWw+uhuw=";
 
   meta = with lib; {
     description = "Terminal UI to browse Hacker News";
