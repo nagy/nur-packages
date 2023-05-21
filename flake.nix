da2c3e55a4b087e177d9bb1aee3bbbbfdaf6a730
Daniel Nagy <danielnagy@posteo.de>
2023-05-21 22:00:00 +0200

    lib: add ssh known public keys

--- flake.nix
+++ flake.nix
@@ -3,8 +3,15 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     {
+      inherit ((import self { pkgs = nixpkgs.legacyPackages."x86_64-linux"; }))
+        lib;
       overlays.default =
         (import self { pkgs = nixpkgs.legacyPackages."x86_64-linux"; }).overlay;
+      nixosModules.ssh-known-keys = { pkgs }: {
+        services.openssh.knownHosts =
+          self.lib.mapAttrs (name: value: { publicKey = value; })
+          self.lib.sshKnownPublicKeys;
+      };
     } // (flake-utils.lib.eachDefaultSystem (system: {
       packages = flake-utils.lib.flattenTree
         (import self { pkgs = nixpkgs.legacyPackages.${system}; });
