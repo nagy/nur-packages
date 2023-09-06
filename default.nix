e44d21e183209d122b015b8d79d375dd32d04966
Daniel Nagy <danielnagy@posteo.de>
2023-09-06 12:15:00 +0200

    treewide: move python packages into own folder

--- default.nix
+++ default.nix
@@ -14,24 +14,24 @@ in lib.makeScope pkgs.newScope (self:
 
     python3Packages = pkgs.recurseIntoAttrs
       (lib.makeScope pkgs.python3Packages.newScope (self: {
-        asyncer = self.callPackage ./pkgs/asyncer { };
-        dbussy = self.callPackage ./pkgs/dbussy { };
-        colorpedia = self.callPackage ./pkgs/colorpedia { };
-        ssort = self.callPackage ./pkgs/ssort { };
-        extcolors = self.callPackage ./pkgs/extcolors { };
-        convcolors = self.callPackage ./pkgs/convcolors { };
-        pymatting = self.callPackage ./pkgs/pymatting { };
-        rembg = self.callPackage ./pkgs/rembg { };
-        warctools = self.callPackage ./pkgs/warctools { };
-        blender-file = self.callPackage ./pkgs/blender-file { };
-        blender-asset-tracer = self.callPackage ./pkgs/blender-asset-tracer { };
-        jtbl = self.callPackage ./pkgs/jtbl { };
-        git-remote-rclone = self.callPackage ./pkgs/git-remote-rclone { };
-        oauth2token = self.callPackage ./pkgs/oauth2token { };
-        images-upload-cli = self.callPackage ./pkgs/images-upload-cli { };
-        imagehash = self.callPackage ./pkgs/imagehash { };
-        pipe21 = self.callPackage ./pkgs/pipe21 { };
-        pystitcher = self.callPackage ./pkgs/pystitcher { };
+        asyncer = self.callPackage ./pkgs/python3-packages/asyncer { };
+        dbussy = self.callPackage ./pkgs/python3-packages/dbussy { };
+        colorpedia = self.callPackage ./pkgs/python3-packages/colorpedia { };
+        ssort = self.callPackage ./pkgs/python3-packages/ssort { };
+        extcolors = self.callPackage ./pkgs/python3-packages/extcolors { };
+        convcolors = self.callPackage ./pkgs/python3-packages/convcolors { };
+        pymatting = self.callPackage ./pkgs/python3-packages/pymatting { };
+        rembg = self.callPackage ./pkgs/python3-packages/rembg { };
+        warctools = self.callPackage ./pkgs/python3-packages/warctools { };
+        blender-file = self.callPackage ./pkgs/python3-packages/blender-file { };
+        blender-asset-tracer = self.callPackage ./pkgs/python3-packages/blender-asset-tracer { };
+        jtbl = self.callPackage ./pkgs/python3-packages/jtbl { };
+        git-remote-rclone = self.callPackage ./pkgs/python3-packages/git-remote-rclone { };
+        oauth2token = self.callPackage ./pkgs/python3-packages/oauth2token { };
+        images-upload-cli = self.callPackage ./pkgs/python3-packages/images-upload-cli { };
+        imagehash = self.callPackage ./pkgs/python3-packages/imagehash { };
+        pipe21 = self.callPackage ./pkgs/python3-packages/pipe21 { };
+        pystitcher = self.callPackage ./pkgs/python3-packages/pystitcher { };
       }));
 
     lispPackages = pkgs.recurseIntoAttrs {
