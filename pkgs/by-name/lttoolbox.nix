a159ad8171eaca04a95d3a37274a6d99c0864bd3
Daniel Nagy <danielnagy@posteo.de>
2025-03-23 19:30:00 +0100

    lttoolbox: refactor

--- pkgs/by-name/lttoolbox.nix
+++ pkgs/by-name/lttoolbox.nix
@@ -1,28 +1,29 @@
-{ stdenv
-, lib
-, fetchFromGitHub
-, autoconf
-, automake
-, libtool
-, libxml2
-, libxslt
-, pkg-config
-, flex
-, pcre
-, pcre-cpp
-, autoreconfHook
-, icu
-, utf8cpp
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  autoconf,
+  automake,
+  libtool,
+  libxml2,
+  libxslt,
+  pkg-config,
+  flex,
+  pcre,
+  pcre-cpp,
+  autoreconfHook,
+  icu,
+  utf8cpp,
 }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation (finalAttrs: {
   pname = "lttoolbox";
   version = "3.7.6";
 
   src = fetchFromGitHub {
     owner = "apertium";
     repo = "lttoolbox";
-    rev = "v${version}";
+    rev = "v${finalAttrs.version}";
     sha256 = "sha256-T92TEhrWwPYW8e49rc0jfM0C3dmNYtuexhO/l5s+tQ0=";
   };
 
@@ -31,17 +32,28 @@ stdenv.mkDerivation rec {
       --replace-fail /usr/include/utf8cpp ${lib.getDev utf8cpp}/include/utf8cpp
   '';
 
-  nativeBuildInputs = [ autoreconfHook pkg-config ];
+  nativeBuildInputs = [
+    autoreconfHook
+    pkg-config
+  ];
 
-  buildInputs =
-    [ autoconf automake libtool libxml2 libxslt flex pcre pcre-cpp icu utf8cpp ];
+  buildInputs = [
+    autoconf
+    automake
+    libtool
+    libxml2
+    libxslt
+    flex
+    pcre
+    pcre-cpp
+    icu
+    utf8cpp
+  ];
 
-  meta = with lib; {
-    description =
-      "Finite state compiler, processor and helper tools used by apertium";
+  meta = {
+    description = "Finite state compiler, processor and helper tools used by apertium";
     homepage = "https://github.com/apertium/lttoolbox";
-
-    license = licenses.gpl2;
-    platforms = platforms.linux;
+    license = lib.licenses.gpl2;
+    platforms = lib.platforms.linux;
   };
-}
+})
