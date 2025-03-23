5d64ac3e7bb642557d52c5acf8675e93c97660ea
Daniel Nagy <danielnagy@posteo.de>
2025-03-23 14:15:00 +0100

    koji: 2.2.0 -> 3.2.0

--- pkgs/by-name/koji.nix
+++ pkgs/by-name/koji.nix
@@ -1,44 +1,51 @@
-{ lib
-, rustPlatform
-, fetchFromGitHub
-, pkg-config
-, libgit2
-, openssl
-, zlib
-, stdenv
-, darwin
-, testers
-, koji
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+  pkg-config,
+  libgit2,
+  openssl,
+  zlib,
+  stdenv,
+  darwin,
+  testers,
+  koji,
 }:
 
 rustPlatform.buildRustPackage rec {
   pname = "koji";
-  version = "2.2.0";
+  version = "3.2.0";
 
   src = fetchFromGitHub {
-    owner = "its-danny";
+    owner = "cococonscious";
     repo = "koji";
-    rev = version;
-    hash = "sha256-2kBjHX7izo4loJ8oyPjE9FtCvUODC3Sm4T8ETIdeGZM=";
+    rev = "v${version}";
+    hash = "sha256-+xtq4btFbOfiyFMDHXo6riSBMhAwTLQFuE91MUHtg5Q=";
   };
 
-  cargoHash = "sha256-owppYDt0YdWoDvfmzVfiIPjLgTAT9eTI1LpRr4Y3XQA=";
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-WiFXDXLJc2ictv29UoRFRpIpAqeJlEBEOvThXhLXLJA=";
 
   nativeBuildInputs = [ pkg-config ];
 
-  buildInputs = [ libgit2 openssl zlib ]
-    ++ lib.optionals stdenv.isDarwin [ darwin.apple_sdk.frameworks.Security ];
+  buildInputs = [
+    libgit2
+    openssl
+    zlib
+  ] ++ lib.optionals stdenv.isDarwin [ darwin.apple_sdk.frameworks.Security ];
 
-  env = { OPENSSL_NO_VENDOR = true; };
+  env = {
+    OPENSSL_NO_VENDOR = true;
+  };
 
+  doCheck = false;
   passthru.tests.version = testers.testVersion { package = koji; };
 
-  meta = with lib; {
+  meta = {
     description = "An interactive CLI for creating conventional commits";
     homepage = "https://github.com/its-danny/koji";
-    changelog =
-      "https://github.com/its-danny/koji/blob/${src.rev}/CHANGELOG.md";
-    license = licenses.mit;
-    maintainers = with maintainers; [ nagy ];
+    changelog = "https://github.com/its-danny/koji/blob/${src.rev}/CHANGELOG.md";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ nagy ];
   };
 }
