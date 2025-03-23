362141e2fb0277ee53738f292052e3db30369088
Daniel Nagy <danielnagy@posteo.de>
2025-03-23 14:29:01 +0100

    hackernews-tui: 0.13.4 -> 0.13.5
    Diff: https://github.com/aome510/hackernews-tui/compare/v0.13.4...v0.13.5

--- pkgs/by-name/hackernews-tui.nix
+++ pkgs/by-name/hackernews-tui.nix
@@ -8,16 +8,17 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "hackernews-tui";
-  version = "0.13.4";
+  version = "0.13.5";
 
   src = fetchFromGitHub {
     owner = "aome510";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-xmqvR1u4owgrZSEmR81I418MHCwn4Ydp9+O6ijEEJ3A=";
+    hash = "sha256-p2MhVM+dbNiWlhvlSKdwXE37dKEaE2JCmT1Ari3b0WI=";
   };
 
-  cargoHash = "sha256-1lNBKWUM4MclZVJi9fT4as9ayamlzcrfeUX6P4nYxag=";
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-KuqAyuU/LOFwvvfplHqq56Df4Dkr5PkUK1Fgeaq1REs=";
 
   passthru.tests.version = testers.testVersion {
     package =
