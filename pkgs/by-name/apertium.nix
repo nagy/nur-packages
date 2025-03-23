793a26694ad9e28d728666187312a1939a0d7edb
Daniel Nagy <danielnagy@posteo.de>
2025-03-23 20:00:00 +0100

    apertium: 3.7.1 -> 3.9.4

--- pkgs/by-name/apertium.nix
+++ pkgs/by-name/apertium.nix
@@ -12,19 +12,20 @@
   pcre,
   pcre-cpp,
   icu,
+  utf8cpp,
   lttoolbox,
   autoreconfHook,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "apertium";
-  version = "3.7.1";
+  version = "3.9.4";
 
   src = fetchFromGitHub {
     owner = "apertium";
     repo = "apertium";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-jns4OI+HjeyTPDPsBB/bONT1vHLzqjBAoHWEB1tymwk=";
+    hash = "sha256-/47A1dYHsFUsIUVPek0pN+bZbANDvwK0elShpIvij78=";
   };
 
   nativeBuildInputs = [
@@ -42,8 +43,17 @@ stdenv.mkDerivation (finalAttrs: {
     pcre
     pcre-cpp
     icu
+    utf8cpp
     lttoolbox
   ];
+
+  enableParallelBuilding = true;
+
+  postPatch = ''
+    substituteInPlace configure.ac \
+      --replace-fail /usr/include/utf8cpp ${lib.getDev utf8cpp}/include/utf8cpp
+    sed -i '/stdc++fs/d' configure.ac
+  '';
 
   meta = {
     description = "Free/open-source machine translation platform";
