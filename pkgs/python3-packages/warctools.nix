cc60d6bf249af1811e7651a0c115c714d0a58c17
Daniel Nagy <danielnagy@posteo.de>
2025-03-31 23:00:00 +0200

    python3Packages.warctools: format

--- pkgs/python3-packages/warctools.nix
+++ pkgs/python3-packages/warctools.nix
@@ -11,16 +11,16 @@ buildPythonPackage rec {
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0w31mv9f5anxgjvjv62w7yyavpp8x11kjyy9yw88ib5q9lknw36f";
+    hash = "0w31mv9f5anxgjvjv62w7yyavpp8x11kjyy9yw88ib5q9lknw36f";
   };
 
   propagatedBuildInputs = [ setuptools ];
 
   doCheck = false;
 
-  meta = with lib; {
+  meta = {
     homepage = "https://github.com/internetarchive/warctools";
     description = "Command line tools and libraries for handling and manipulating WARC files (and HTTP contents)";
-    license = licenses.mit;
+    license = lib.licenses.mit;
   };
 }
