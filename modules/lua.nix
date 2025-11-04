84637f077a79d4546b6cd53eb83137c3823f02c4
Daniel Nagy <danielnagy@posteo.de>
2025-11-04 21:45:00 +0100

    modules/lua: update

--- modules/lua.nix
+++ modules/lua.nix
@@ -1,4 +1,9 @@
-{ pkgs, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
 let
   cfg = config.nagy.lua;
@@ -30,5 +35,28 @@ in
       magicOrExtension = "lua";
       interpreter = pkgs.lua5_4.interpreter;
     };
+
+    # This block can unfortunately not be used because luac
+    # outputs it internal addresses which cause a different output every time.
+    # For Example:
+    # -function <file.lua:6,12> (28 instructions at 0x26134230)
+    # +function <file.lua:6,12> (28 instructions at 0x2c511220)
+    #
+    # programs.git = {
+    #   config = {
+    #     diff = {
+    #       luac = {
+    #         textconv = "${pkgs.lua5_4}/bin/luac -p -l -";
+    #         binary = true;
+    #       };
+    #     };
+    #   };
+    # };
+    # environment.etc.gitattributes = {
+    #   text = lib.mkAfter ''
+    #     *.luac diff=luac
+    #   '';
+    # };
+
   };
 }
