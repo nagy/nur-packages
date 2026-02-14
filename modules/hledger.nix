1e77664fb72a6de26c206af1ac0dad459f95ea2b
Daniel Nagy <danielnagy@posteo.de>
2026-02-14 23:00:00 +0100

    modules/hledger: remove enable switch for now

--- modules/hledger.nix
+++ modules/hledger.nix
@@ -1,30 +1,16 @@
-{
-  config,
-  pkgs,
-  lib,
-  # nur,
-  ...
-}:
+{ pkgs, ... }:
 
 let
-  cfg = config.nagy.hledger;
   self = import ../. { inherit pkgs; };
 in
 {
+  environment.systemPackages = [
+    pkgs.hledger
+    pkgs.hledger-ui
+    pkgs.hledger-web
+    # nur.repos.nagy.hledger-fmt
+    self.hledger-fmt
+  ];
 
-  options.nagy.hledger = {
-    enable = lib.mkEnableOption "hledger config";
-  };
-
-  config = lib.mkIf cfg.enable {
-    environment.systemPackages = [
-      pkgs.hledger
-      pkgs.hledger-ui
-      pkgs.hledger-web
-      # nur.repos.nagy.hledger-fmt
-      self.hledger-fmt
-    ];
-
-    # environment.sessionVariables.LEDGER_FILE = "...";
-  };
+  # environment.sessionVariables.LEDGER_FILE = "...";
 }
