99c050caae7306876ccf10931a5dd0c92fba0540
Daniel Nagy <danielnagy@posteo.de>
2026-02-14 19:30:00 +0100

    lib/hledger.nix: update

--- lib/hledger.nix
+++ lib/hledger.nix
@@ -6,26 +6,31 @@
 
 {
   mkHledgerFromCSV =
-    {
-      root,
-      hledger ? pkgs.hledger,
-    }:
+    { root }:
     pkgs.stdenv.mkDerivation {
       name = "journal";
-      nativeBuildInputs = [ hledger ];
 
       src = fs.toSource {
         root = root;
         fileset = fs.fileFilter (file: file.hasExt "csv" || file.hasExt "CSV" || file.hasExt "rules") root;
       };
 
-      env.LANG = "en_US.UTF-8";
-      env.LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
+      nativeBuildInputs = [ pkgs.hledger ];
+
+      env = {
+        LANG = "en_US.UTF-8";
+        LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
+      };
+
       buildPhase = ''
+        runHook preBuild
+
         hledger \
           $(printf -- "--file %s " **/*.csv **/*.CSV) \
           $(printf -- "--rules %s " *.rules) \
           print > $out
+
+        runHook postBuild
       '';
 
       preferLocalBuild = true;
