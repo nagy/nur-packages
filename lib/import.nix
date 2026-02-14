baad1a6a7ed2740f6065636628b2b5406927791f
Daniel Nagy <danielnagy@posteo.de>
2026-02-14 19:45:00 +0100

    lib/import.nix: init

---
+++ lib/import.nix
@@ -0,0 +1,31 @@
+{
+  pkgs,
+  lib ? pkgs.lib,
+}:
+
+let
+  selflib = (import ./.. { inherit pkgs; }).lib;
+  conversionList = [
+    # Rust
+    {
+      test = selflib.isRustFile;
+      importer = selflib.importRust;
+    }
+  ];
+in
+rec {
+
+  findImporter =
+    file:
+    (lib.findFirst
+      # Predicate
+      (el: if el.test file then true else false)
+      # Default value
+      { importer = builtins.import; }
+      # List to search
+      conversionList
+    ).importer;
+
+  import = file: (findImporter file) file;
+
+}
