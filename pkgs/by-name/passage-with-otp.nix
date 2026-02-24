ac7bd03a9286bf2cabdf20ffecf48e05ca625a10
Daniel Nagy <danielnagy@posteo.de>
2026-02-24 07:15:00 +0100

    passage-with-otp: use `self` rather than rely on `nur` argument

--- pkgs/by-name/passage-with-otp.nix
+++ pkgs/by-name/passage-with-otp.nix
@@ -1,13 +1,16 @@
 {
+  pkgs,
   passage,
-  nur,
   ...
 }:
 
+let
+  self = (import ../.. { inherit pkgs; });
+in
 passage.overrideAttrs (old: {
   postInstall = ''
     ${old.postInstall}
     install -Dm755 -t $out/lib/passage/extensions \
-      ${nur.repos.nagy.pass-otp-unstable}/lib/password-store/extensions/otp.bash
+      ${self.pass-otp-unstable}/lib/password-store/extensions/otp.bash
   '';
 })
